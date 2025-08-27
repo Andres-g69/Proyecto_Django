d4e628ab757821e4f24de2c66fc9eb98026469bc
alumnos nunoa <alumnosnunoa@inacap.cl>
2025-08-27 10:53:55 -0400

    modelos de datos actualizados

--- miapp/models.py
+++ miapp/models.py
@@ -6,10 +6,20 @@ class Cliente(models.Model):
     cli_direccion = models.CharField(max_length=60, null=True, blank=True)
     cli_telefono = models.CharField(max_length=24, null=True, blank=True)
     cli_mail = models.CharField(max_length=60, null=True, blank=True)
-    
+
+class Meta:
+    db_table ='CLIENTE'
+
+def __str__(self):
+    return self.cli_nombre or f"Cliente{self.id_cliente}"
+
 class Orden(models.Model):
     id_orden = models.AutoField(primary_key=True)
     id_cliente = models.ForeignKey('Cliente', on_delete=models.CASCADE, to_field='id_cliente')
     ord_fecha = models.DateField(null=True, blank=True)
 
-    
+class Meta:
+    db_table= 'ORDEN'
+
+def __str__(self):
+    return f"Orden{self.id_orden}"
